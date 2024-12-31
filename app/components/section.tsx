80223e41be7809cf1148839b5d7866804583fc9b
nikachu2012 <webmaster@nikachu.net>
2025-01-01 00:20:45 +0900

    [add] 見出しのみのセクションを追加

--- app/components/section.tsx
+++ app/components/section.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export default function Section({ title, children, className }: { title: ReactNode, children: ReactNode, className?: string }) {
+export function Section({ title, children, className }: { title: ReactNode, children: ReactNode, className?: string }) {
     return <div className={`bg-neutral-800 border border-neutral-700 rounded px-5 py-3 ${className}`}>
         <h2 className="w-full font-bold text-xl mb-2">
             {title}
@@ -9,4 +9,10 @@ export default function Section({ title, children, className }: { title: ReactNo
             {children}
         </div>
     </div>
+}
+
+export function SectionHeader({ children }: { children: ReactNode }) {
+    return <h2 className="w-full font-bold text-xl">
+        {children}
+    </h2>
 }

15e6a98be9d24ffbb086fc72dd79e0d96ec29447
nikachu2012 <webmaster@nikachu.net>
2025-01-01 00:13:36 +0900

    [add] ヘッダにトップページへのリンクを追加

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
     return <header>
         <div className="flex mt-2 md:my-5 mb-5 flex-col gap-2 md:flex-row md:items-end">
-            <span className="font-[900] text-3xl leading-none">情報工学同好会</span>
-            <span className="font-[900] text-2xl leading-none">(非公認)</span>
+            <Link href={"/"}>
+                <span className="font-[900] text-3xl leading-none">情報工学同好会</span>
+                <span className="font-[900] text-2xl leading-none ml-2">(非公認)</span>
+            </Link>
         </div>
     </header>;
 }

a825776cb4a6362402487819bd6bb7f392d7808e
nikachu2012 <webmaster@nikachu.net>
2025-01-02 16:24:36 +0900

    [fix] フッターの下にマージンを追加

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 
 export default function Footer() {
-    return <footer className="mt-3">
+    return <footer className="my-3">
         Copyright (C) Computer Science Club (unofficial) in NITNC All rights reserved.
     </footer>;
 }

65d912f8fb2089cc868b02ff36f34849ae051eab
nikachu2012 <webmaster@nikachu.net>
2025-01-03 19:30:57 +0900

    [add] 各種APIを追加

---
+++ app/api/goods/[id]/route.ts
@@ -0,0 +1,22 @@
+import { DATA_PATH } from "@/app/utils/dataConst";
+import { existsSync, readFileSync } from "fs";
+import { join } from "path";
+
+export const dynamic = 'force-static'
+
+export async function GET(
+    request: Request,
+    { params }: { params: Promise<{ id: string }> }
+) {
+    const id = (await params).id;
+
+    const path = join(DATA_PATH, "goods", id + ".json");
+
+    if (!existsSync(path)) {
+        return Response.json({}, { status: 400 })
+    }
+
+    // file exists
+    const f = JSON.parse(readFileSync(path, "utf-8")) as goods;
+    return Response.json(f)
+}

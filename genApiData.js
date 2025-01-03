887f8fedc5104251bba0e3b60663dcf9dcee101a
nikachu2012 <webmaster@nikachu.net>
2025-01-04 03:45:51 +0900

    [add] 正誤情報、イベント、販売品リストを生成するように変更

--- genApiData.js
+++ genApiData.js
@@ -1,10 +1,31 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const { unlinkSync } = require("fs");
+const { unlinkSync, readdirSync, writeFileSync } = require("fs");
 const { copySync } = require("fs-extra");
-const { join } = require("path");
+const { join, parse } = require("path");
 const { cwd, exit } = require("process");
+
+
+const listFiles = (dir) => readdirSync(dir, { withFileTypes: true }).flatMap(dirent =>
+    dirent.isFile() ? [join(dir, dirent.name)] : listFiles(join(dir, dirent.name))
+)
+
+const genIdList = (name) => {
+    const fl = listFiles(join(cwd(), "public", "data", name))
+    const idlist = fl.map(e => parse(e).name);
+
+    const result = {}
+    result[name] = idlist;
+
+    writeFileSync(join(cwd(), "public", "data", name + ".json"), JSON.stringify(result))
+}
 
 copySync(join(cwd(), "data"), join(cwd(), "public", "data"))
 
+// 各種リストの生成
+genIdList("errata")
+genIdList("event")
+genIdList("goods")
+
+// readme.mdの削除
 unlinkSync(join(cwd(), "public", "data", "readme.md"))
 exit(0);

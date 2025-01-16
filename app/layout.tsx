9752ac0a2c30159f5be3ac14ceb1e8c2adb05b30
nikachu2012 <webmaster@nikachu.net>
2025-01-16 21:57:49 +0900

    [add] OGP画像の設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,23 @@ export const metadata: Metadata = {
     default: "情報工学同好会 (非公認)"
   },
   description: "高専生によって作られた同人グループ「沼津高専 情報工学同好会 (非公認)」の公式サイトです",
-  authors: []
+  authors: [],
+  openGraph: {
+    images: [
+      {
+        url: "https://csc.nikachu.net/ogp.png",  // Must be an absolute URL
+        width: 1200,
+        height: 675,
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: '情報工学同好会 (非公認)',
+    description: '高専生によって作られた同人グループ「沼津高専 情報工学同好会 (非公認)」の公式サイトです',
+    creator: '@csc_a452p',
+    images: ["https://csc.nikachu.net/ogp.png"], // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({
